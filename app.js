a4c69ef091b21fd14acb027994db184884f1cff7
nyc0lai <nyc0lai@gmail.com>
2021-03-17 18:09:08 +0200

    add a coment in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//create a variable off type constant with name kelvin
+//today forecast value assign to const. kelvin
 const kelvin = 293;
-//converting kelvin to celsius
+//converting kelvin to celsius and assign value to "celsius" variable
 let celsius = kelvin-273;
 let fahrenheit = Math.floor(celsius*(9/5)+32);
 console.log(fahrenheit);
